1ff8dce3537e0a8ca3ea24f840e8bf5e3540bacb
Nirmal Bharadwaj <mits2015.nirmal@gmail.com>
2019-01-04 11:58:30 +0530

    iterators_2_finished

--- loops/iteratorSet_2.py
+++ loops/iteratorSet_2.py
@@ -6,6 +6,8 @@ import itertools as itert
 
 ## cycle
 it = [1,2,3]
+it1 = [4,5,6]
+li1 = [ (1, 10, 5), (8, 4, 1), (5, 4, 9), (11, 10 , 1) ]
 # itert.cycle(it) ## print it elements in cycle infinitely
 
 ## repeat
@@ -20,4 +22,22 @@ print(list(itert.combinations_with_replacement(it,2))) ## combinations for list 
 ### cartesian product with two iterators
 print("product :",list(itert.product(it,it2)))
 
-###
+### islice -- slicling iterable object
+print("slice",list(itert.islice(it,1,3,1)))
+
+### starmap -- apply a fuunction on tuple list
+print("star map:",list(itert.starmap(min,li1)))
+
+### takewhile -- prints the values of iterable till it is false
+print("takewhile:",list(itert.takewhile(lambda x:x%2==0,it)))
+
+### tee -- splits the container into no of iterators
+print("tee:",list(itert.tee(it,2)))
+
+it_ = itert.tee(it,2)
+for  l in range(0,2):
+      print(list(it_[l]))
+
+## zip-longest
+
+print("zip_longest",print(list(itert.zip_longest(it,it1,"_"))))
